84c7a9772294f6ace2ff713d9cac83df030db913
Prabhu Yellina <Prabhu Yellina>
2016-11-20 16:18:49 -0600

    Added Common Followers Functionality

--- assignments/twitter1.py
+++ assignments/twitter1.py
@@ -16,13 +16,26 @@ class Twitter:
             for teweet in user_timeline:
                 if key_word in teweet['text']:
                     print teweet['text']
-        except TwythonError as err:
+        except Exception  as err:
             print err
+            exit(1)
 
     def get_followes_list(self, name):
         followers = self.twiter.get_followers_list(screen_name=name)
-        print [i["screen_name"]for i in followers["users"]]
+        #print [i["screen_name"]for i in followers["users"]]
+        self.list1=[i["screen_name"]for i in followers["users"]]
+        return self.list1
+
+    def get_common_followers(self, name1, name2):
+        followers_name1=self.get_followes_list(name1)
+        followers_name2=self.get_followes_list(name2)
+#        list_common = [ followers_name2(member) for member in followers_name1 if member in followers_name2 ]
+        list_common=list(set(followers_name1) & set(followers_name2))
+        print list_common
+
 
 obj = Twitter()
-obj.get_data('RGVzoomin', 'Children')
-obj.get_followes_list('RGVzoomin')
+#obj.get_data('RGVzoomin', 'Children')
+#obj.get_followes_list('RGVzoomin')
+#obj.get_followes_list('purijagan')
+obj.get_common_followers('RGVzoomin', 'purijagan')
